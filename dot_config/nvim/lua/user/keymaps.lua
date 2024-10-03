3a194bc085e34d986d4d67ecb35b6b9dce0e1ee0
Nebulea <github@nebulea.dev>
2024-10-03 08:30:54 +0200

    monthly update time <3 (idk what I added i'm sorry, LSP n stuff)

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -45,3 +45,15 @@ keymap("n", "<leader>w", ":lua vim.wo.wrap = not vim.wo.wrap<CR>", opts)
 
 
 vim.api.nvim_set_keymap('t', '<C-;>', '<C-\\><C-n>', opts)
+
+keymap(
+  "i",
+  "<C-f>",
+  function () return vim.fn['codeium#Accept']() end,
+  { desc = "Accept codeium suggestion", expr = true, silent = true })
+
+keymap(
+  "n",
+  "<leader>l",
+  "<cmd>lua vim.diagnostic.open_float()<cr>",
+  { desc = "Toggle lsp_lines" })
--- dot_config/nvim/lua/user/mason-lspconfig.lua
+++ dot_config/nvim/lua/user/mason-lspconfig.lua
@@ -8,8 +8,35 @@ local M = {
     local mason_lspconfig = require("mason-lspconfig")
       mason_lspconfig.setup({
         ensure_installed = {
+          -- Lua
           "lua_ls",
+
+          -- C
+          --"clangd",
+
+          -- Rust
           "rust_analyzer",
+
+          -- Python
+          "pyright",
+
+          -- Web
+          "tsserver",
+          "html",
+          "cssls",
+
+          -- Markdown
+          "marksman",
+
+          -- JSON & YAML
+          "jsonc",
+          "jsonls",
+          "taplo",
+          "yamlls",
+
+          -- Docker and docker compose
+          "dockerls",
+          "docker_compose_language_service",
         }
       })
       mason_lspconfig.setup_handlers {
